f87d9baa7a96ebd4454f36fde4e728b84163acfd
Cesar Sanchez <84400955+cesarsnchz13@users.noreply.github.com>
2022-01-01 21:11:40 -0600

    Update dishes.router.js

--- src/dishes/dishes.router.js
+++ src/dishes/dishes.router.js
@@ -1,19 +1,13 @@
 const router = require("express").Router();
 const controller = require("./dishes.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
-//import the correct files. refer to older projects.
 
-// TODO: Implement the /dishes routes needed to make the tests pass
-// add two routes: /dishes, and /dishes/:dishId and attach the handlers (create, read, update, and list) exported from src/dishes/dishes.controller.js.
-
-// create route for ('/:dishId') for get, put,
 router
   .route("/:dishId")
   .get(controller.read)
   .put(controller.update)
   .all(methodNotAllowed);
 
-// create route for ('/') for get, post and all,
 router
   .route("/")
   .get(controller.list)
